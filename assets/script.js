e5d6f18fb9738dcc05e84a6fa3c805bb46852794
Natalie <natalie@bigorangemedia.co.uk>
2023-01-29 11:38:59 +0000

    add loop for forecast

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         event.preventDefault();
         searchString = $('#search-input').val();
         geoCode = geoURL + searchString + '&appid=' + key;
-        
+
 
         // To use geocode API to get lon and lat from city search -parse the results to the next ajax query
         $.ajax({
@@ -29,7 +29,6 @@ $(document).ready(function () {
 
             lat = startResponse.coord.lat;
             lon = startResponse.coord.lon;
-            console.log('this is the lat ' + lat + ' this is the lon ' + lon);
             queryURL = apiURL + 'lat=' + lat + '&lon=' + lon + '&appid=' + key;
 
             $.ajax({
@@ -40,53 +39,64 @@ $(document).ready(function () {
                 // pass response of list objects to new variable to shorten code later
                 var results = fullResponse.list[0];
                 // converting Kelvin temp to celcius, with 2 decimal points
-                var temp = (results.main.temp - 273.15).toFixed(2);
-                var wind = results.wind.speed;
-                var humid = results.main.humidity;
+                var currentTemp = (results.main.temp - 273.15).toFixed(2);
+                var currentWind = results.wind.speed;
+                var currentHumid = results.main.humidity;
                 // grab icon data then pass it through a variable- as comes in parts!
-                var icon = results.weather[0].icon;
-                var iconURL = 'https://openweathermap.org/img/wn/' + icon + '.png';
+                var currentIcon = results.weather[0].icon;
+                var iconURL = 'https://openweathermap.org/img/wn/' + currentIcon + '.png';
 
-                
                 var weatherToday = $('<div>');
                 weatherToday.addClass('col-12 border p-4');
 
                 var cityToday = $('<h2 class="text-capitalize">');
                 cityToday.text(searchString + ' (' + todaysDate + ') ');
 
-                var displayIcon = $('<img>');
-                displayIcon.attr('src', iconURL);
-                displayIcon.attr('title', results.weather[0].description);
+                var displayIcon = $('<img>').attr('src', iconURL).attr('title', results.weather[0].description);
 
                 var listWeather = $('<ul class="list-unstyled">');
-                var listTemp = $('<li class="pb-2">').text('Temp: ' + temp + ' ℃');
-                var listWind = $('<li class="pb-2">').text('Wind: ' + wind + ' KPH');
-                var listHumid = $('<li class="pb-2">').text('Humidity: ' + humid + ' %');
+                var listTemp = $('<li class="pb-2">').text('Temp: ' + currentTemp + ' ℃');
+                var listWind = $('<li class="pb-2">').text('Wind: ' + currentWind + ' KPH');
+                var listHumid = $('<li class="pb-2">').text('Humidity: ' + currentHumid + ' %');
 
+                //append items
                 weatherToday.append(cityToday);
                 cityToday.append(displayIcon);
                 weatherToday.append(listWeather);
                 listWeather.append(listTemp, listWind, listHumid);
 
-                //append items
                 $('#today').append(weatherToday);
 
-                //for loop to go through the other 5 day forecast
-                for (var i = 0; i < 5; i++) {
-                    var forecast = results[i];
-                    
+
+                // for loop that iterates through the next five days - checks when the time matches noon and prints iterations only from that time
+                for (var i = 0; i < fullResponse.list.length; i++) {
+                    if (fullResponse.list[i].dt_txt.endsWith('12:00:00')) {
+                        var nextDay = $('<ul class="list-unstyled">');
+                        // using the moment unix method we can convert and format the dt 'seconds' to the date - the dt is represented as seconds from 1st jan 1970.
+                        var nextDayDate = $('<li class="pb-2">').text(moment.unix(fullResponse.list[i].dt).format('DD/MM/YYYY'));
+                        var nextDayTemp = fullResponse.list[i].main.temp;
+                        var nextDayTempConvert = $('<li class="pb-2"><h2>').text((nextDayTemp - 273.15).toFixed(2));
+                        var nextDayIcon = fullResponse.list[i].weather[0].icon;
+                        var nextDayIconURL = 'https://openweathermap.org/img/wn/' + nextDayIcon + '.png';
+
+                        var nextDayIconDisplay = $('<img>').attr('src', nextDayIconURL).attr('title', fullResponse.list[i].weather[0].description);
+
+                        //append items
+                        $('#forecast').append(nextDay);
+
+                        nextDay.append(nextDayDate, nextDayIconDisplay, nextDayTempConvert);
+                    }
                 }
+
             });
 
-            console.log(geoCode);
-    console.log(queryURL);
 
         });
 
 
     });
 
-    
+
 
     // List the previous searched cities - using local storage
 
